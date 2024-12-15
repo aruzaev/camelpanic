a1112b119ee9e4ffd7b27f3c76775cfb2a338703
aruzaev <ruzaev.artem21@gmail.com>
2024-12-14 19:51:57 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://www.example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    sanity({
+      projectId: "jrbn2020",
+      dataset: "production",
+      useCdn: false,
+    }),
+  ],
 });
